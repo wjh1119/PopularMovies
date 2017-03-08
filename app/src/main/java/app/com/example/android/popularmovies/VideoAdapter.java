177cee89dbe65871b8239f4c1678da33a5bb9921
wjh2016 <524022279@qq.com>
2017-03-08 19:58:30 +0800

    2.58

--- app/src/main/java/app/com/example/android/popularmovies/VideoAdapter.java
+++ app/src/main/java/app/com/example/android/popularmovies/VideoAdapter.java
@@ -74,7 +74,7 @@ public class VideoAdapter extends BaseAdapter {
 
         viewHolder.numberTextView.setText(video.get("number").toString());
         viewHolder.nameTextView.setText(video.get("name").toString());
-        viewHolder.playImageView.setImageResource(R.drawable.playvido);
+        viewHolder.playImageView.setImageResource(R.drawable.playvideo);
         viewHolder.playImageView.setOnClickListener(new View.OnClickListener() {
 
             @Override
